2149c9b5a945c264efb17be8b61de65dbd8bd3a3
john <kiokoj81@gmail.com>
2022-11-28 22:56:04 +0300

    Setup

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={a0de6f3a41b9064f0e2456edad8552b0}
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
