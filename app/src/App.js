58e744df6a7fa0977d775dce57908ad0deb6f088
john <kiokoj81@gmail.com>
2022-12-01 19:34:18 +0300

    Fetch and display data from coinCap Api

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
-
+import Crytpos from './Components/Crytpos';
+import Crypto from './Components/Crypto';
+import NavBar from './Components/NavBar';
+import DetailsPage from './Components/DetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <>
+            <NavBar />
+            <Crytpos />
+          </>
+        )}
+      />
+      <Route
+        path="/crypto-details"
+        element={(
+          <>
+            <DetailsPage />
+            <Crypto />
+          </>
+        )}
+      />
+    </Routes>
   );
 }
 
